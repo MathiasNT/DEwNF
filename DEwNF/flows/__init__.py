316792a30c350667b1d8b149f7ed5eb682d9fa52
Mathias N Tygesen <mnt@beepanalytics.com>
2020-03-02 20:23:21 +0100

    updated inits

---
+++ DEwNF/flows/__init__.py
@@ -0,0 +1,9 @@
+from .ConditionalAffineCoupling import ConditionalAffineCoupling, ConditionedAffineCoupling, conditional_affine_coupling
+from .ConditionalNormalizingFlowWrapper import ConditionalNormalizingFlowWrapper
+
+__all__ = [
+    'conditional_affine_coupling',
+    'ConditionedAffineCoupling',
+    'ConditionalAffineCoupling',
+    'ConditionalNormalizingFlowWrapper'
+]
---
+++ DEwNF/utils/__init__.py
@@ -0,0 +1,5 @@
+from .PlottingUtils import plot_4_contexts_cond_flow
+
+__all__ = [
+    'plot_4_contexts_cond_flow'
+]
