f16dc94e2575ea13119418d08afe4aa68dffe590
Mathias N Tygesen <mnt@beepanalytics.com>
2020-03-02 20:39:22 +0100

    update init

--- DEwNF/utils/__init__.py
+++ DEwNF/utils/__init__.py
@@ -1,5 +1,7 @@
-from .PlottingUtils import plot_4_contexts_cond_flow
+from .PlottingUtils import plot_4_contexts_cond_flow, plot_loss, sliding_plot_loss
 
 __all__ = [
-    'plot_4_contexts_cond_flow'
+    'plot_4_contexts_cond_flow',
+    'plot_loss',
+    'sliding_plot_loss'
 ]
