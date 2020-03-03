1b8792f8a56467f10c888319d9a9b6f2b18b2a06
Mathias N Tygesen <mnt@beepanalytics.com>
2020-03-03 13:24:19 +0100

    typos and init

--- DEwNF/__init__.py
+++ DEwNF/__init__.py
@@ -1,9 +1,11 @@
 import DEwNF.flows
 import DEwNF.utils
 import DEwNF.samplers
+import DEwNF.regularizers
 
 __all__ = [
     'flows',
     'samplers',
-    'utils'
+    'utils',
+    'regularizers'
 ]
