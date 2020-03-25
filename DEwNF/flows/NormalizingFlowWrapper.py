bea8f3a785754864db2835c7938f14541509270a
Mathias N Tygesen <mnt@beepanalytics.com>
2020-03-25 11:24:59 +0100

    added factories

--- DEwNF/flows/NormalizingFlowWrapper.py
+++ DEwNF/flows/NormalizingFlowWrapper.py
@@ -1,5 +1,8 @@
 import torch.distributions as dist
 from torch import nn
+import torch
+from pyro.distributions.transforms import affine_coupling, permute
+import itertools
 
 
 class NormalizingFlowWrapper(object):
@@ -9,3 +12,27 @@ class NormalizingFlowWrapper(object):
 
     def cuda(self):
         self.modules.cuda()
+
+
+def normalizing_flow_factory(flow_depth, problem_dim, c_net_depth, c_net_h_dim, cuda):
+    # We define the base distribution
+    if cuda:
+        base_dist = dist.Normal(torch.zeros(problem_dim).cuda(), torch.ones(problem_dim).cuda())
+    else:
+        base_dist = dist.Normal(torch.zeros(problem_dim), torch.ones(problem_dim))
+
+    # We define the transformations
+    transforms = [affine_coupling(input_dim=problem_dim,
+                                  hidden_dims=[c_net_h_dim for i in range(c_net_depth)]) for i in range(flow_depth)]
+
+    # We need to permute dimensions to affect them both THIS NEEDS A FIX
+    perms = [permute(2, torch.tensor([1, 0])) for i in range(flow_depth)]
+
+    # We sandwich the AffineCouplings and permutes together. Unelegant hotfix to remove last permute but it works
+    flows = list(itertools.chain(*zip(transforms, perms)))[:-1]
+
+    # We define the normalizing flow wrapper
+    normalizing_flow = NormalizingFlowWrapper(transforms, flows, base_dist)
+    if cuda:
+        normalizing_flow.cuda()
+    return normalizing_flow
--- DEwNF/flows/__init__.py
+++ DEwNF/flows/__init__.py
@@ -1,11 +1,13 @@
 from .ConditionalAffineCoupling import ConditionalAffineCoupling, ConditionedAffineCoupling, conditional_affine_coupling
-from .ConditionalNormalizingFlowWrapper import ConditionalNormalizingFlowWrapper
-from .NormalizingFlowWrapper import NormalizingFlowWrapper
+from .ConditionalNormalizingFlowWrapper import ConditionalNormalizingFlowWrapper, conditional_normalizing_flow_factory
+from .NormalizingFlowWrapper import NormalizingFlowWrapper, normalizing_flow_factory
 
 __all__ = [
     'conditional_affine_coupling',
     'ConditionedAffineCoupling',
     'ConditionalAffineCoupling',
     'ConditionalNormalizingFlowWrapper',
-    'NormalizingFlowWrapper'
+    'conditional_normalizing_flow_factory',
+    'NormalizingFlowWrapper',
+    'normalizing_flow_factory'
 ]
