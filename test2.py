0107439e60be6f9d9d82249521185a7e2560c8a9
Mathias N Tygesen <mnt@beepanalytics.com>
2020-04-05 16:46:53 +0200

    test2 updated

--- test2.py
+++ test2.py
@@ -2,7 +2,6 @@ import argparse
 import pickle
 import os
 import pandas as pd
-from tqdm import tqdm
 
 from DEwNF.flows import ConditionalAffineCoupling2, ConditionedAffineCoupling2, ConditionalNormalizingFlowWrapper, conditional_affine_coupling2, normalizing_flow_factory, conditional_normalizing_flow_factory2
 from DEwNF.utils import plot_4_contexts_cond_flow, plot_loss, sliding_plot_loss, plot_samples, plot_train_results, split_on_days
@@ -10,7 +9,6 @@ from DEwNF.samplers import RotatingTwoMoonsConditionalSampler
 from DEwNF.regularizers import NoiseRegularizer, rule_of_thumb_noise_schedule, approx_rule_of_thumb_noise_schedule, square_root_noise_schedule, constant_regularization_schedule
 
 import torch.optim as optim
-import torch
 
 
 def main(args):
@@ -19,8 +17,107 @@ def main(args):
 
     print(cuda_exp)
 
-    tensortest = torch.tensor([1, 1]).cuda()
-    print(tensortest)
+    # Notebook experiment settings
+    experiment_name = args.experiment_name
+    experiment_results_folder = args.results_folder
+    results_path = os.path.join("../", experiment_results_folder)
+    data_folder = args.data_folder
+    data_file = args.data_file
+
+    # Regularization settings
+    if args.noise_reg_scheduler == "constant":
+        noise_reg_schedule = constant_regularization_schedule
+    elif args.noise_reg_scheduler == "sqrt":
+        noise_reg_schedule = square_root_noise_schedule
+    elif args.noise_reg_scheduler == "rot":
+        noise_reg_schedule = rule_of_thumb_noise_schedule
+    else:
+        noise_reg_schedule = constant_regularization_schedule
+
+    noise_reg_sigma = args.noise_reg_sigma  # Used as sigma in rule of thumb and as noise in const
+
+    # Data settings
+    obs_cols = args.obs_cols
+    context_cols = args.context_cols
+
+    # Training settings
+    epochs = args.epochs
+    batch_size = args.epochs
+
+    # Dimensions of problem
+    problem_dim = len(args.obs_cols)
+    context_dim = len(args.context_cols)
+
+    # Flow settings
+    flow_depth = args.flow_depth
+    c_net_depth = args.c_net_depth
+    c_net_h_dim = args.c_net_h_dim
+
+    # Define context conditioner
+    context_n_depth = args.context_n_depth
+    context_n_h_dim = args.context_n_h_dim
+    rich_context_dim = args.rich_context_dim
+
+    settings_dict = {
+        "epochs": epochs,
+        "batch_size": batch_size,
+        "problem_dim": problem_dim,
+        "context_dim": context_dim,
+        "flow_depth": flow_depth,
+        "c_net_depth": c_net_depth,
+        "c_net_h_dim": c_net_h_dim,
+        "context_n_depth": context_n_depth,
+        "context_n_h_dim": context_n_h_dim,
+        "rich_context_dim": rich_context_dim,
+        "obs_cols": obs_cols,
+        "context_cols": context_cols
+    }
+
+    print(f"Settings:\n{settings_dict}")
+
+    # Load data
+    csv_path = os.path.join(data_folder, data_file)
+    donkey_df = pd.read_csv(csv_path, parse_dates=[4, 11])
+
+    train_dataloader, test_dataloader = split_on_days(donkey_df, obs_cols, context_cols, batch_size, cuda_exp)
+
+    # Define stuff for reqularization
+    data_size = len(train_dataloader)
+    data_dim = problem_dim + context_dim
+
+    # Define normalizing flow
+    normalizing_flow = conditional_normalizing_flow_factory2(flow_depth=flow_depth,
+                                                             problem_dim=problem_dim,
+                                                             c_net_depth=c_net_depth,
+                                                             c_net_h_dim=c_net_h_dim,
+                                                             context_dim=context_dim,
+                                                             context_n_h_dim=context_n_h_dim,
+                                                             context_n_depth=context_n_depth,
+                                                             rich_context_dim=rich_context_dim,
+                                                             cuda=cuda_exp)
+
+    # Setup Optimizer
+    optimizer = optim.Adam(normalizing_flow.modules.parameters(), lr=1e-4)
+    print("number of params: ", sum(p.numel() for p in normalizing_flow.modules.parameters()))
+
+    # Setup regularization
+    h = noise_reg_schedule(data_size, data_dim, noise_reg_sigma)
+    noise_reg = NoiseRegularizer(discrete_dims=None, h=h, cuda=cuda_exp)
+    print(f"Data size: {train_dataloader.dataset.shape}")
+    print(f"Noise scale: {h}")
+
+    # Train and test sizes
+    n_train = train_dataloader.dataset.shape[0]
+    n_test = test_dataloader.dataset.shape[0]
+    print(f"n_train {n_train}")
+    print(f"n_test {n_test}")
+
+    # Training loop
+    full_train_losses = []
+    train_losses = []
+    test_losses = []
+    no_noise_losses = []
+
     print("finished")
 
 
