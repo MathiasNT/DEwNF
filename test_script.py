46083d469e3900ba762bdb839eb395d7115d554d
Mathias N Tygesen <mnt@beepanalytics.com>
2020-04-05 12:21:07 +0200

    pickle test

--- test_script.py
+++ test_script.py
@@ -1,13 +1,18 @@
 import argparse
 import DEwNF
+import pickle
 
 
-def main(test):
+def main(test, name):
     print(test)
+    file_name = "name.pickle"
+    with open(file_name, "wb") as f:
+        pickle.dump(test, f)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--test", help="testing stuff")
+    parser.add_argument("--name", help="Name of the experiment, used in naming outputs")
     args = parser.parse_args()
-    main(args.test)
+    main(args.test, name=args.name)
