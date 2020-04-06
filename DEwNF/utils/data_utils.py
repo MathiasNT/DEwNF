cc0225d5ff8d8190707ef81c4677bde907d28bdc
Mathias N Tygesen <mnt@beepanalytics.com>
2020-04-06 09:19:18 +0200

    fixed dataloaders

--- DEwNF/utils/data_utils.py
+++ DEwNF/utils/data_utils.py
@@ -20,7 +20,7 @@ def split_on_days(df, obs_cols, context_cols, batch_size, cuda_exp):
     # Transform training data
     scaled_train_obs = obs_scaler.transform(df.loc[train_idx, obs_cols])
     scaled_train_context = context_scaler.transform(df.loc[train_idx, context_cols])
-    scaled_trained_data = torch.cat((torch.tensor(scaled_train_obs), torch.tensor(scaled_train_context)), dim=1).type(torch.FloatTensor)
+    scaled_train_data = torch.cat((torch.tensor(scaled_train_obs), torch.tensor(scaled_train_context)), dim=1).type(torch.FloatTensor)
 
     # Transform test data
     scaled_test_obs = obs_scaler.transform(df.loc[test_idx, obs_cols])
@@ -29,31 +29,46 @@ def split_on_days(df, obs_cols, context_cols, batch_size, cuda_exp):
 
     # Send to cuda if necessary
     if cuda_exp:
-        scaled_trained_data = scaled_trained_data.cuda()
+        scaled_train_data = scaled_train_data.cuda()
         scaled_test_data = scaled_test_data.cuda()
 
     # Wrap in dataloaders to take care of batching
-    train_dataloader = DataLoader(scaled_trained_data, batch_size=batch_size, shuffle=True)
+    train_dataloader = DataLoader(scaled_train_data, batch_size=batch_size, shuffle=True)
     test_dataloader = DataLoader(scaled_test_data, batch_size=batch_size)
 
     return train_dataloader, test_dataloader
 
 
 def split_synthetic(df, batch_size, data_size, cuda_exp):
-    # Take subset of data
+    # Calculate split id
+    split_id = int(data_size * 0.8)
+
+    # Take subset of data and split
     df = df[:data_size]
+    train_df = df[:split_id]
+    test_df = df[split_id:]
 
-    # Normalize data and send to cuda
-    obs_scaler = StandardScaler().fit(df.iloc[:, 0:2])
-    context_scaler = StandardScaler().fit(df.iloc[:, 2:])
-    scaled_obs = obs_scaler.transform(df.iloc[:, 0:2])
-    scaled_context = context_scaler.transform(df.iloc[:, 2:])
-    scaled_data = torch.cat((torch.tensor(scaled_obs), torch.tensor(scaled_context)), dim=1).type(torch.FloatTensor)
-    data_tensors = scaled_data
+    # Fit transform
+    obs_scaler = StandardScaler().fit(train_df.iloc[:, 0:2])
+    context_scaler = StandardScaler().fit(train_df.iloc[:, 2:])
 
-    # Split into test and train and put in DataLoader
-    data_len = data_tensors.shape[0]
-    data_split_id = int(data_len * 0.8)
-    train_dataloader = DataLoader(data_tensors[:data_split_id], batch_size=batch_size, shuffle=True)
-    test_dataloader = DataLoader(data_tensors[data_split_id:], batch_size=batch_size)
+    # Transform training data
+    scaled_train_obs = obs_scaler.transform(train_df.loc[:, 0:2])
+    scaled_train_context = context_scaler.transform(train_df.loc[:, 2:])
+    scaled_train_data = torch.cat((torch.tensor(scaled_train_obs), torch.tensor(scaled_train_context)), dim=1).type(torch.FloatTensor)
+
+    # Transform test data
+    scaled_test_obs = obs_scaler.transform(test_df.loc[:, 0:2])
+    scaled_test_context = context_scaler.transform(test_df.loc[:, 2:])
+    scaled_test_data = torch.cat((torch.tensor(scaled_test_obs), torch.tensor(scaled_test_context)), dim=1).type(torch.FloatTensor)
+
+    # Send to cuda if necessary
+    if cuda_exp:
+        scaled_train_data = scaled_train_data.cuda()
+        scaled_test_data = scaled_test_data.cuda()
+
+    # Wrap in dataloaders to take care of batching
+    train_dataloader = DataLoader(scaled_train_data, batch_size=batch_size, shuffle=True)
+    test_dataloader = DataLoader(scaled_test_data, batch_size=batch_size)
+
     return train_dataloader, test_dataloader
